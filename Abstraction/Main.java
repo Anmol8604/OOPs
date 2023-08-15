45f6222d75a7d9505525c0360fdeb0c3f67f3938
Anmol8604 <anmol.12018604@lpu.in>
2023-08-15 21:44:27 +0530

    oops

---
+++ Abstraction/Main.java
@@ -0,0 +1,5 @@
+package Abstraction;
+
+// Abstraction: in abstraction be hide unnecessary details and only showing essential information.
+public class Main {
+}
---
+++ Encapsulation/Main.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+// Encapsulation: Wrapping up of data members and methods in a class.
+// It hides all the code and properties so that it can be protected from an outside.
+
+// e.g interfaces
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
